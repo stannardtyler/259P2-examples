65fde14bf97ee3b661f4d016bf8cb77e5d8ee307
stannardtyler <tstannard64@gmail.com>
2024-10-28 17:02:30 -0700

    Add files via upload

--- button/sketch.js
+++ button/sketch.js
@@ -1,37 +1,57 @@
 let button;
+let button2;
+let bg;
 
-// let button2;
+function stateOne() {
+  bg = color(0, 0, 0);
+}
+
+function stateTwo() {
+  bg = color(255, 255, 255);
+}
 
 function setup() {
   createCanvas(400, 400);
+  noStroke();
+  bg = color(220, 220, 220);
 
-  button = new Button(200, 200, 50, 100);
+  button = new Button(100, 300, 50, 100, stateOne);
 
-  // button2 = new Button(100, 300, 50, 50);
+  button2 = new Button(300, 300, 50, 50, stateTwo);
 }
 
 function draw() {
-  background(220);
+  background(bg);
 
   button.show();
   button.rollover(mouseX, mouseY);
 
-  // button2.show();
-  // button2.rollover(mouseX, mouseY);
+  button2.show();
+  button2.rollover(mouseX, mouseY);
+}
+
+function mousePressed() {
+  if (button.rollover(mouseX, mouseY)) {
+    button.buttonFunction();
+  }
+
+  if (button2.rollover(mouseX, mouseY)) {
+    button2.buttonFunction();
+  }
 }
 
 class Button {
-  constructor(tempX, tempY, tempW, tempH) {
+  constructor(tempX, tempY, tempW, tempH, tempFunction) {
     this.x = tempX;
     this.y = tempY;
     this.w = tempW;
     this.h = tempH;
     this.color = 0;
+    this.buttonFunction = tempFunction;
   }
 
   rollover(px, py) {
-    let d = dist(this.x, this.y, px, py);
-
+    //checks the vertical and horizontal edges independently
     if (
       px > this.x - this.w / 2 &&
       px < this.x + this.w / 2 &&
@@ -39,8 +59,10 @@ class Button {
       py < this.y + this.h / 2
     ) {
       this.color = 255;
+      return true;
     } else {
       this.color = 0;
+      return false;
     }
   }
 
